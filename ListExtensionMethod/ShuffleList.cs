030c4d51ed15ea05b0321ee6bb35032d18b1fcf9
Çağlar Çavdar <caglar.cavdar@windowslive.com>
2021-02-18 23:25:01 +0300

    Initial commit

---
+++ ListExtensionMethod/ShuffleList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListExtensionMethod
+{
+    public static class ShuffleList
+    {
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            if (list.Count != 0)
+            {
+                var rnd = new Random();
+                var newList = list.OrderBy(x => rnd.Next()).ToList();
+                for (var i = 0; i < list.Count; i++)
+                {
+                    list[i] = newList[i];
+                }
+            }
+        }
+    }
+}
